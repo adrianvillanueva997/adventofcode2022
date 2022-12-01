8697596d48670723c238bfaa74f62748a2458a54
adrianvillanueva997 <adrianvillanueva997@gmail.com>
2022-12-01 10:36:49 +0100

    Day 1 part 1 complete

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,25 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
 fn main() {
-    println!("Hello, world!");
+    let file = File::open("input.txt").unwrap();
+    let reader = BufReader::new(file);
+    let lines = reader.lines();
+    let mut current_count = 0;
+    let mut current_max = 0;
+    for line in lines {
+        let line_content = line.unwrap();
+        if line_content.is_empty() {
+            if current_count > current_max {
+                current_max = current_count;
+            }
+            current_count = 0;
+        } else {
+            let line_number: i32 = line_content.parse().unwrap();
+            current_count += line_number;
+        }
+    }
+    println!("{}", current_max);
 }
