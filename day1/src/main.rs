a627b5a727787e60b1c34c0db7a2b572a06a0e60
adrianvillanueva997 <adrianvillanueva997@gmail.com>
2022-12-01 11:02:34 +0100

    Day one challenge 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -8,18 +8,19 @@ fn main() {
     let reader = BufReader::new(file);
     let lines = reader.lines();
     let mut current_count = 0;
-    let mut current_max = 0;
+    let mut v: Vec<i32> = Vec::new();
     for line in lines {
         let line_content = line.unwrap();
         if line_content.is_empty() {
-            if current_count > current_max {
-                current_max = current_count;
-            }
+            v.push(current_count);
             current_count = 0;
         } else {
             let line_number: i32 = line_content.parse().unwrap();
             current_count += line_number;
         }
     }
-    println!("{}", current_max);
+    v.sort();
+    v.reverse();
+    let total = v[0] + v[1] + v[2];
+    println!("{}", total);
 }
