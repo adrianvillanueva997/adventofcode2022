0e059ddeae70d97435e1687f3b6e40ed33335cea
adrianvillanueva997 <adrianvillanueva997@gmail.com>
2022-12-02 13:46:35 +0100

    First part ready

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,10 +1,52 @@
 use std::{
     fs::File,
-    io::{BufRead, BufReader},
+    io::{BufRead, BufReader, Error, ErrorKind::InvalidInput},
 };
 
-fn calculate_score(human_play: &str, ai_play: &str) -> i32 {
-    0
+fn convert_ai_input(ai_play: &str) -> Result<i32, Error> {
+    match ai_play {
+        "A" => Ok(1),
+        "B" => Ok(2),
+        "C" => Ok(3),
+        _ => Err(Error::new(InvalidInput, "Invalid input")),
+    }
+}
+
+fn convert_human_input(human_play: &str) -> Result<i32, Error> {
+    match human_play {
+        "X" => Ok(1),
+        "Y" => Ok(2),
+        "Z" => Ok(3),
+        _ => Err(Error::new(InvalidInput, "Invalid Input!")),
+    }
+}
+
+fn get_option_score(human_play: i32) -> i32 {
+    match human_play {
+        1 => 1,
+        2 => 2,
+        3 => 3,
+        _ => 0,
+    }
+}
+/// 1=> rock 2=>paper 3=> scissors
+fn calculate_round_score(human_play: i32, ai_play: i32) -> Result<i32, Error> {
+    let mut score_round = 0;
+    score_round += get_option_score(human_play);
+    let options = (human_play, ai_play);
+    match options {
+        (1, 1) => Ok(score_round + 3),
+        (1, 2) => Ok(score_round),
+        (1, 3) => Ok(score_round + 6),
+        (2, 1) => Ok(score_round + 6),
+        (2, 2) => Ok(score_round + 3),
+        (2, 3) => Ok(score_round),
+        (3, 1) => Ok(score_round),
+        (3, 2) => Ok(score_round + 6),
+        (3, 3) => Ok(score_round + 3),
+
+        _ => Err(Error::new(InvalidInput, "Something happened")),
+    }
 }
 
 fn main() {
@@ -15,9 +57,9 @@ fn main() {
     for line in lines {
         let line_content = line.unwrap();
         let plays: Vec<&str> = line_content.split(' ').collect();
-        let human_play = plays[0];
-        let ai_play = plays[1];
-        let round_score = calculate_score(human_play, ai_play);
+        let human_play = convert_human_input(plays[1]).unwrap();
+        let ai_play = convert_ai_input(plays[0]).unwrap();
+        let round_score = calculate_round_score(human_play, ai_play).unwrap();
         human_score += round_score;
     }
     println!("{}", human_score);
